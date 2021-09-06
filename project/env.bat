3af6e7c6b7286891161046b26fbfbcc7e44b9c25
Noah Cohn <noahc42@gmail.com>
2021-09-05 20:39:20 -0700

    Delete env.bat

--- project/env.bat
+++
@@ -1,8 +0,0 @@
-call C:/emscripten/emsdk/emsdk_env.bat
-set PATH=C:\msys64\mingw64\bin;%PATH%
-
-set TOOLCHAIN_FILE="C:/emscripten/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake"
-set BUILD_DIR="C:/WebAssembly/WEBGL_EGL_base/binaries"
-set CMAKE_FILE_DIR="C:/WebAssembly/WEBGL_EGL_base/"
-set INSTALL_DIR="C:/WebAssembly/WEBGL_EGL_base/bin"
-set EMSCRIPTEN_INCLUDE_DIR="C:/emscripten/emsdk/upstream/emscripten/system/include"
