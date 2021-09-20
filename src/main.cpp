a4abaaa42d5d5b12ca8ee5dfe6ec3bf6b9372d6e
Noah Cohn <noahc42@gmail.com>
2021-09-20 13:15:46 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -40,16 +40,16 @@ EGLDisplay display=eglGetDisplay(EGL_DEFAULT_DISPLAY);
 eglInitialize(display,&major,&minor);
 if(eglChooseConfig(display,attribute_list,&eglconfig,1,&config_size)==EGL_TRUE && eglconfig!=NULL){
 if(eglBindAPI(EGL_OPENGL_ES_API)!=EGL_TRUE){
-printjs("EglBindAPI failed");
+EM_ASM({console.log("EglBindAPI failed");})
 }
 EGLint anEglCtxAttribs2[]={EGL_CONTEXT_CLIENT_VERSION,3,EGL_NONE,EGL_NONE};
 
 contextegl=eglCreateContext (display,eglconfig,EGL_NO_CONTEXT,anEglCtxAttribs2);
 
 if(contextegl==EGL_NO_CONTEXT){
-printjs("eglCreateContext failed.");
+EM_ASM({console.log("eglCreateContext failed.");})
 }else{
-printjs("eglCreateContext success!");
+EM_ASM({console.log("eglCreateContext success!");})
 EGLSurface surface=eglCreateWindowSurface(display,eglconfig,NULL,NULL);
 eglMakeCurrent(display,surface,surface,contextegl);
 glClearColor(1.0,0.0,0.0,0.5);
