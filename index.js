3fb42d8dfe5512442db5e7077995e632db72287e
Noah Cohn <noahc42@gmail.com>
2021-09-20 16:46:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // const OccViewerModuleInitialized = createModule(MyModule);
 
 // var Module.canvas=document.getElementById('canvas');
-var canvas=document.getElementById('canvas');
+// var canvas=document.getElementById('canvas');
 
 
 // var Module={canvas:(function(){canvas=document.getElementById('canvas');return canvas;})()};
