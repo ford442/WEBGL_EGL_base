326271ad16e2134f87705ad6de729c07a12e02b9
Noah Cohn <noahc42@gmail.com>
2021-09-20 19:21:03 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 var mod={canvas:(function(){var can=document.getElementById('canvas');return can;})(),};
 const OccViewerModuleInitialized=createModule(mod);
+document.getElementById('btn2').addEventListener('click',function(){
+mod.ccall('jsdraw');
+});
