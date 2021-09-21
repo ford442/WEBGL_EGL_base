6f303e482fda36c72fb61e0db88e37e4497e60df
Noah Cohn <noahc42@gmail.com>
2021-09-21 13:25:07 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var MyModule =
 {
     canvas: (function() {
     var aCanvas = document.getElementById('canvas');
+  //  aGlCtx = aCanvas.getContext ('webgl2',  { alpha: true, premultipliedAlpha: false, depth: false, antialias: false, preserveDrawingBuffer: false } ); 
     return aCanvas;
   })(),
 
