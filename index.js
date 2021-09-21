21b71380d5c08b9a718683f81e9d5908b709390b
Noah Cohn <noahc42@gmail.com>
2021-09-20 17:31:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 // var Module.canvas=document.getElementById('canvas');
 // var canvas=document.getElementById('canvas');
-var Module={canvas:(function(){let acanvas=document.getElementById('canvas');return acanvas;})()};
+var Module={canvas:(function(){let acanvas=document.getElementById('canvas');return acanvas;})(),
+};
 
 // var Module={canvas:(function(){canvas=document.getElementById('canvas');return canvas;})()};
