be1cbcb8c3c86c3196bc80b69ff417dea132add7
Noah Cohn <noahc42@gmail.com>
2021-09-20 18:26:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,14 +9,17 @@
 
 // var Module={canvas:(function(){canvas=document.getElementById('canvas');return canvas;})()};
 
-var Module =
+var MyModule =
 {
     canvas: (function() {
     var aCanvas = document.getElementById('canvas');
+    aGlCtx = aCanvas.getContext ('webgl',  { alpha: false, depth: true, antialias: false, preserveDrawingBuffer: true } ); 
     return aCanvas;
   })(),
 
 };
+
+const OccViewerModuleInitialized = createModule(MyModule);
 
 
 
